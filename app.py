227caed0282789a91bf9d507b961d4acdc3f01de
Bayan Berri <bberri@stuy.edu>
2017-11-11 13:20:05 -0500

    app.py

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ import json
 
 
 app = Flask(__name__)
-data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=cQGPw3WcqhhqGcvkbjnH5nTux27lcWYsZ0YfK8gE")
+
 #data= data.read()
 
 
 @app.route("/")
 def root():
+    data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=cQGPw3WcqhhqGcvkbjnH5nTux27lcWYsZ0YfK8gE")
     print "\n\n~~~~~~~~~~~~~~printing data.geturl~~~~~~~~~~~~~~~~\n\n"
-    #print data.geturl()
+    print data.geturl()
     print "\n\n\n ~~~~~~~~~~~~~~ printing info()~~~~~~~~~~~~~~~~~~~~"
-    #print data.info()
+    print data.info()
     print "\n\n\n ~~~~~~~~~~~~~~ printing read()"
     string= data.read()
     print string
     d= json.loads(string)
-    print "adeeb thinks he's cool"
-   # d= str(d);
+
     return render_template("temp.html", Pic=d["url"],explanation=d["explanation"])
 #return data.read()
     '''

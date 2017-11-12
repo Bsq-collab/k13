65c4e7f3ffe25e3c5837655a67817314235a909b
Bayan Berri <bberri@stuy.edu>
2017-11-11 19:14:32 -0500

    comments

--- app.py
+++ app.py
@@ -13,20 +13,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
+    #opens the URL to be read by my program
     data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=cQGPw3WcqhhqGcvkbjnH5nTux27lcWYsZ0YfK8gE")
     print "\n\n~~~~~~~~~~~~~~printing data.geturl~~~~~~~~~~~~~~~~\n\n"
-    print data.geturl()
+    print data.geturl() #returns the actual url in case of redirects
     print "\n\n\n ~~~~~~~~~~~~~~ printing info()~~~~~~~~~~~~~~~~~~~~"
-    print data.info()
+    print data.info() # returns https header information
     print "\n\n\n ~~~~~~~~~~~~~~ printing read()"
-    string= data.read()
+    string= data.read()# returns contents of the target webpage
     print string
-    d= json.loads(string)
+    d= json.loads(string)#turns a json object string into a dictionary
 
     currencyKey= urllib2.urlopen("http://apilayer.net/api/live?access_key=0fec2f60496646d29dcf96603bcc07f0")
-    currstr=currencyKey.read()
-    dictionary=json.loads(currstr)
-    newdict= dictionary["quotes"]
+    currstr=currencyKey.read() #opens the url to be read by program
+    dictionary=json.loads(currstr) #converts the string to a dictionary
+    newdict= dictionary["quotes"] #quotes key is a dictionary of currency conversions
 
     return render_template("temp.html", Pic=d["url"],explanation=d["explanation"],di=newdict)
 
